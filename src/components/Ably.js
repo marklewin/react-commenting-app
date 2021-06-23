735d1825ba6d84d6e983cb2b19ef0f3c97675c03
marklewin <mark.lewin@ably.com>
2021-06-23 11:06:21 +0100

    First commit

---
+++ src/components/Ably.js
@@ -0,0 +1,3 @@
+import { Realtime } from "ably/browser/static/ably-commonjs.js"
+
+export default new Realtime("krrNCg.VPmXdw:j-9q1WOueznF1i6q")
---
+++ src/components/CommentBox.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react"
+import Ably from "./Ably"
+
+class CommentBox extends Component {
+  constructor(props) {
+    super(props)
+    this.addComment = this.addComment.bind(this)
+  }
+  render() {
+    return (
+      <div>
+        <h1 className="title">Kindly leave your thoughts below</h1>
+        <form onSubmit={this.addComment}>
+          <div className="field">
+            <div className="control">
+              <input
+                type="text"
+                className="input"
+                name="name"
+                placeholder="Your name"
+              />
+            </div>
+          </div>
+          <div className="field">
+            <div className="control">
+              <textarea
+                className="textarea"
+                name="comment"
+                placeholder="Add a comment"
+              ></textarea>
+            </div>
+          </div>
+          <div className="field">
+            <div className="control">
+              <button className="button is-primary">Submit</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    )
+  }
+  addComment(e) {
+    // Prevent the default behaviour of form submit
+    e.preventDefault()
+    // Get the value of the comment box
+    // and make sure it not some empty strings
+    const comment = e.target.elements.comment.value.trim()
+    const name = e.target.elements.name.value.trim()
+    // Get the current time.
+    const timestamp = Date.now()
+    // Make sure name and comment boxes are filled
+    if (name && comment) {
+      const commentObject = { name, comment, timestamp }
+      // Publish comment
+      const channel = Ably.channels.get("comments")
+      channel.publish("add_comment", commentObject, (err) => {
+        if (err) {
+          console.log("Unable to publish message; err = " + err.message)
+        }
+      })
+      // Clear input fields
+      e.target.elements.comment.value = ""
+    }
+  }
+}
+
+export default CommentBox
